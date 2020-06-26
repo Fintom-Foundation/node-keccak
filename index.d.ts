4ae04d3704906d9ad10bbe64a561bd7de779cafd
RayL <ray.li.cmd@gmail.com>
2020-06-27 00:07:28 +0800

    update package info

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+const sha3 = require('./build/Release/sha3');
+
+export interface SHA3Hash {
+  /**
+   * Update the SHA3 Hash Content
+   * @param input The Input String or Buffer
+   */
+  update(input?: string | Buffer): SHA3Hash;
+
+  /**
+   * Digest the SHA3 Hash Content
+   * @param format The Hash Output Format (default as binary)
+   */
+  digest(format?: 'binary' | 'hex'): string;
+}
+
+export var SHA3Hash: {
+  /**
+   * The Constructor for creating a byte array (default is SHA3-512)
+   */
+  new(bytes?: 224 | 256 | 384 | 512): SHA3Hash;
+} = sha3.SHA3Hash;
